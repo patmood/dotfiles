03908f64d67630854916b4a5cd6ad7cde1c52b85
patmood <patrick.n.moody@gmail.com>
2014-05-15 09:21:58 -0700

    add fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,8 @@
+rvm use ruby-1.9.3
+setenv EDITOR vim
+
+alias bai="cd /Users/patrickmoody/verdigris/building.ai"
+alias gs="git status"
+alias gc="git commit -v"
+alias gd="git diff"
+alias ga="git add"
