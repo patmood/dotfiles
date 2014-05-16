fc18c7bfe715d0728be4714695d2d027b9beb93d
patmood <patrick.n.moody@gmail.com>
2014-05-15 17:48:53 -0700

    aussie ascii

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,13 @@
 rvm use ruby-1.9.3
 setenv EDITOR vim
 
+function fish_greeting
+  echo -e " ,-_|\ \n/     \ \n\\_,-._/ \n     v"
+end
+
 alias bai="cd /Users/patrickmoody/verdigris/building.ai"
 alias gs="git status"
 alias gc="git commit -v"
 alias gd="git diff"
 alias ga="git add"
+
